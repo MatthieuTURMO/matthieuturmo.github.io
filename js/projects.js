da9b71084f32c12b31b9f33ff07508ebd4d2a7b8
unknown <MatthieuTURMO>
2017-10-18 16:40:17 +0200

    ajout de jqueryui, et refonte des cartes dans l index

---
+++ js/projects.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    var windowHeight = $(window).height();
+    var scrollTopLevel = $(document).scrollTop();
+    showProjet(windowHeight, scrollTopLevel, 50, '.projet');
+});
+
+$(document).scroll(function () {
+    var windowHeight = $(window).height();
+    var scrollTopLevel = $(document).scrollTop();
+    showProjet(windowHeight, scrollTopLevel, 50, '.projet');
+});
+
+
+// met à jour progress bar, utiles dans la vue about
+function showProjet(windowHeight, scrollTopLevel, offset, selector) {
+    var positionBottomScreen = windowHeight + scrollTopLevel;
+    $(selector).each(function (index) {
+        var positionProjet = $(this).offset().top;
+        console.log('bot', positionBottomScreen);
+        console.log('topdiv', positionProjet);
+        if (positionBottomScreen >= positionProjet + offset) {
+            $(this).animate({
+                opacity: '1'
+            }, 1000);
+        }
+    });
+
+};
