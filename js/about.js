99ef4162a94e64feb6ddc0f233c8200f4b5861f8
turmz <matthieu.turmo@free.fr>
2017-10-23 14:32:52 +0200

    ajout de la transition sur les studies dans about

--- js/about.js
+++ js/about.js
@@ -2,6 +2,7 @@ $(document).scroll(function () {
     var windowHeight = $(window).height();
     var scrollTopLevel = $(document).scrollTop();
     setProgressBar(windowHeight, scrollTopLevel, 40, '#skills');
+    setStudies(windowHeight, scrollTopLevel, 100, '#studies');
 });
 
 // met à jour progress bar, utiles dans la vue about
@@ -17,5 +18,17 @@ function setProgressBar(windowHeight, scrollTopLevel, offset, selector) {
             var finalWidth = $(this).attr('finalWidth');
             $(this).css('width', finalWidth);
         });
+    }
+};
+
+// met à jour progress bar, utiles dans la vue about
+function setStudies(windowHeight, scrollTopLevel, offset, selector) {
+    var positionBottomScreen = windowHeight + scrollTopLevel;
+    // on récupère la position du top de la div
+    var positionSkills = $(selector).position();
+    var positionSkillsTop = positionSkills.top;
+    // si le niveau de scroll est supérieur, on met à jour les progress bar
+    if (positionBottomScreen >= positionSkillsTop+offset) {
+        $('#studies>.row>div').css('opacity', '1');
     }
 };
