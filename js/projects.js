6395ed7393fad6bcea8cbc96518f5192d8232adc
turmz <matthieu.turmo@free.fr>
2018-02-06 15:55:51 +0100

    changement francais navbar

--- js/projects.js
+++ js/projects.js
@@ -10,8 +10,14 @@ $(document).scroll(function () {
     showProjet(windowHeight, scrollTopLevel, 50, '.projet');
 });
 
+$(window).resize(function () {
+    var windowHeight = $(window).height();
+    var scrollTopLevel = $(document).scrollTop();
+    showProjet(windowHeight, scrollTopLevel, 50, '.projet');
+});
 
-// met à jour progress bar, utiles dans la vue about
+
+// affiche les projets en fonction du niveau de scroll
 function showProjet(windowHeight, scrollTopLevel, offset, selector) {
     var positionBottomScreen = windowHeight + scrollTopLevel;
     $(selector).each(function (index) {
